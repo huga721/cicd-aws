d17f5d3dcf2af4283bf3a300f2d6c80cf8aa94f8
huga721 <szabahubert@gmail.com>
2024-02-28 12:43:41 +0100

    init commit

---
+++ src/main/java/p/hubert/springcicdaws/SpringCicdAwsApplication.java
@@ -0,0 +1,13 @@
+package p.hubert.springcicdaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCicdAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/p/hubert/springcicdaws/SpringCicdAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package p.hubert.springcicdaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
